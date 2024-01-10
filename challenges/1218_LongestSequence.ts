10b8e1834cd40057a6291e0649b49887bfdda313
Delioos <htz_j@protonmail.com>
2024-01-10 09:46:54 +0100

    fix(1218): refacto to functionnal prog -> no more timeout

--- challenges/1218_LongestSequence.ts
+++ challenges/1218_LongestSequence.ts
@@ -1,5 +1,23 @@
 function longestSubSequence(arr: number[], diff: number): number {
   let maxSeqLength = 0;
+  const seqLengths = new Map<number, number>();
+
+  for (const currNum of arr) {
+    const prevNum = currNum - diff;
+    const prevSeqLength = seqLengths.get(prevNum) || 0; // if prevNum is not in seqLengths, default to 0
+    const currSeqLength = prevSeqLength + 1;
+
+    seqLengths.set(currNum, currSeqLength);
+    maxSeqLength = Math.max(maxSeqLength, currSeqLength);
+  }
+
+  return maxSeqLength;
+}
+
+
+/* old and unoptimized solution:
+function longestSubSequence(arr: number[], diff: number): number {
+  let maxSeqLength = 0;
   let tempSeqLength;
 
   for (let i = 1; i < arr.length; i++) {
@@ -16,4 +34,4 @@ function longestSubSequence(arr: number[], diff: number): number {
     }
   }
     return maxSeqLength;
-}
+}*/
